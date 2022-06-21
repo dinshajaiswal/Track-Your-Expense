9c9b5121ad3e77d24c2d967ed2289b024462fbe4
Dinsha <dinsha.jaiswal123@gmail.com>
2022-06-21 18:15:02 +0530

    new commit

---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Card, CardHeader, CardContent, Typography} from '@material-ui/core'
+import { Doughnut } from 'react-chartjs-2'
+import useTransactions from '../../useTransactions'
+import useStyles from './styles'
+ import {Chart, ArcElement} from 'chart.js'
+Chart.register(ArcElement);
+
+const Details = ({title,subheader}) => {
+  const { total, chartData } = useTransactions(title);
+  const classes = useStyles();
+
+  return (
+    <Card className={title === 'Income' ? classes.income : classes.expense}>
+      <CardHeader title={title} subheader={subheader} />
+      <CardContent>
+        <Typography variant="h5">${total}</Typography>
+        <Doughnut data={chartData} />
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Details
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from 'react'
+import ReactDom from 'react-dom'
+import { Provider } from './context/context'
+import { SpeechProvider } from '@speechly/react-client'
+import App from './App'
+import './index.css'
+ReactDom.render(
+    <SpeechProvider appId='51465732-ea7e-4bf5-b4bc-2cfd9655ed33' language='en-US' >
+        <Provider>
+        <App/>
+        </Provider>
+    </SpeechProvider>
+,document.getElementById('root'));
